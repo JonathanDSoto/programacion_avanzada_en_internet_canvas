76cffd82144256fa9e06d22e55d3fd5a2ea718bc
Jonathan Soto <jsoto@uabcs.mx>
2020-08-14 18:15:48 -0600

    branch turno vespertino

--- index.php
+++ index.php
@@ -1,81 +1,90 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Canvas</title>
+	<title>
+		Canvas
+	</title>
 	<style type="text/css">
-		.content{
+		.container{
 			width: 80%;
-			min-height: 600px;
 			background-color: gray;
 			margin: auto;
-		}
+			min-height: 500px;
+		} 
 		canvas{
-			background-color: white;
-			margin:auto;
+			background-color: #F7D358;
+		}
+		img{
+			visibility: hidden;
 		}
 	</style>
 </head>
 <body>
-	<div class="content">
-		<canvas id="canvas" width="500" height="250" ></canvas>
-		<img id="imagen" style="visibility: hidden;" src="burbujas.png">
+	<div class="container">
+		<canvas id="canvas" width="400" height="300">
+			Opps tu navegador no soporta CANVAS
+		</canvas>
+		<img id="pingu" src="bubujas.png">
 	</div>
 	<script type="text/javascript">
-		var canvas = document.getElementById('canvas');
-		if( canvas.getContext )
-		{
-			var ctx = canvas.getContext('2d');
+		window.onload = function(){
+			var canvas = document.getElementById('canvas');
+			if (canvas.getContext) {
+				var ctx = canvas.getContext('2d');
 
-			// ctx.fillStyle = 'rgb(0,0,200)';
-			// ctx.fillRect(0,0,100,50);
+				//cuadrado
+				// ctx.fillStyle = "rgb(255,0,0)";
+				// ctx.fillRect (10, 10, 55, 50);
 
-			// ctx.fillStyle = 'rgba(200,0,0,0.5)';
-			// ctx.fillRect(20,20,100,50);
+				// ctx.fillStyle = "rgba(0,0,200,0.5)";
+				// ctx.fillRect(30,30,55,50);
 
-			//linea
-			// ctx.moveTo(0,0);
-			// ctx.lineTo(100,100); 
-			// ctx.stroke();
+				// ctx.strokeStyle = "green";
+				// ctx.strokeRect(70,70,55,50);
 
-			//circulo
-			// ctx.beginPath();
-			// ctx.arc(150, 150, 40, 0, 2 * Math.PI);
-			// ctx.stroke();
-			// ctx.closePath();
+				//linea
+				// ctx.moveTo(0,0);
+				// ctx.lineTo(200,100);
+				// ctx.lineTo(50,70);
+				// ctx.lineTo(100,20);
+				// ctx.lineTo(0,0)
+				// ctx.fill();
 
-			//Texto
-			// ctx.font = "30px Arial";
-			// ctx.strokeStyle = "#FFBF00";
-			// ctx.strokeText("Hola mundo",10,30);
+				//circulo
+				// ctx.beginPath();
+				// ctx.fillStyle = "green";
+				// ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+				// ctx.fill();
+				// ctx.closePath();
 
-			//grandiante
-			// var grd = ctx.createLinearGradient(0, 0, 200, 0);
-			// grd.addColorStop(0,"blue");
-			// grd.addColorStop(1,"white");
+				//texto
+				// ctx.font = "30px Arial";
+				// ctx.strokeText("Hola mundo",10,50);
 
-			// ctx.fillStyle = grd;
-			// ctx.fillRect(10,10,150,80);
+				//linear Gradiant
+				// var grd = ctx.createLinearGradient(0,0,200,0);
+				// grd.addColorStop(0,"red");
+				// grd.addColorStop(1,"white");
 
-			//grandiante circular
-			// var grd = ctx.createRadialGradient(75, 75, 1, 120, 90, 100)
-			// grd.addColorStop(0,"blue");
-			// grd.addColorStop(1,"white"); 
+				// ctx.fillStyle = grd;
+				// ctx.fillRect (10, 10, 150, 80);
 
-			// ctx.fillStyle = grd;
-			// ctx.fillRect(10,10,150,150);
+				//radial Gradiant
+				// var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+				// grd.addColorStop(0,"red");
+				// grd.addColorStop(1,"white");
 
-			//imagen
-			var img = document.getElementById('imagen');
-			ctx.drawImage(img,10,10);
+				// ctx.fillStyle = grd;
+				// ctx.fillRect(10, 10, 150, 80);
+
+
+				//imagen
+				var img = document.getElementById('pingu');
+				ctx.drawImage(img,0,0);
+			}
 		}
-		
+
+		 
 	</script>
 </body>
 </html>
-
-
-
-
-
-
-
