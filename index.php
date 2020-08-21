25024e4b7b1d40f59fc72a0694a9f161fee105c4
Jonathan Soto <jsoto@uabcs.mx>
2020-08-20 20:02:49 -0600

    eventos teclado y moviendo al usuario

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<style type="text/css">
 		.content{
 			width: 80%;
-			min-height: 600px;
+			min-height: 200px;
 			background-color: gray;
 			margin: auto;
 		}
@@ -25,6 +25,7 @@
 	</div>
 	<script type="text/javascript">
 		var canvas = null, ctx = null, x = 0, y = 0;
+		var lastPress = 65, speed = 5, player = null, food =null;
 
 		window.requestAnimationFrame = (function(){
 			return window.requestAnimationFrame || 
@@ -41,14 +42,49 @@
 			ctx.fillRect(0,0,canvas.width,canvas.height)
 
 			ctx.fillStyle = "#0f0";
-			ctx.fillRect(x,y,10,10);
+			player.paint(ctx);
+
+			ctx.fillStyle = "red";
+			food.paint(ctx);
+
+			// ctx.fillStyle = "#0f0";
+			// ctx.fillRect(x,y,10,10);
 		}
 
 		function act(){
+ 
 
-			x += 2;
-			if(x > canvas.width){
-				x = -10;
+			if(lastPress==65 || lastPress == 37){ //izquierda
+				player.x -= speed;
+				if (player.x < 0 ) {
+					player.x = canvas.width;
+				}
+			}
+
+			if(lastPress==68 || lastPress == 39){ //derecha
+				player.x += speed;
+				if(player.x > canvas.width){
+					player.x = -10;
+				}
+			}
+
+			if(lastPress==87 || lastPress == 38){ //arriba
+				player.y -= speed;
+				if (player.y < 0) {
+					player.y = canvas.height;
+				}
+			}
+
+			if(lastPress==83 || lastPress == 40){ //abajo
+				player.y += speed;
+				if (player.y > canvas.height) {
+					player.y = -10;
+				}
+			} 
+
+			if(player.intersects(food)){
+				food.x = random(canvas.width);
+				food.y = random(canvas.height);
 			}
 		}
 
@@ -61,11 +97,41 @@
 		function init(){
 			canvas = document.getElementById('canvas');
 			ctx = canvas.getContext('2d');
+
+			player = new Rectangle(0,0,10,10);
+			food = new Rectangle(70,50,10,10);
+
 			run();
 		} 
 		
 		window.addEventListener('load',init,false);
 		
+		document.addEventListener('keydown',function(e){ 
+			if(e.keyCode==65 || e.keyCode ==  37 || e.keyCode ==  68 || e.keyCode ==  39 || e.keyCode ==  87 || e.keyCode ==  38 || e.keyCode ==  83 || e.keyCode ==  40 )
+				lastPress = e.keyCode; 
+		})
+
+		function Rectangle(x,y,w,h){
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+
+			this.paint = function(ctx){
+				ctx.fillRect(this.x,this.y,this.w,this.h);
+			}
+
+			this.intersects = function(rect){
+				if (this.x < rect.x + rect.w && this.x + this.w > rect.x && 
+					this.y < rect.y + rect.h && this.y + this.h > rect.y){
+					return true;
+				}
+			}
+		}
+
+		function random(m){
+			return Math.floor(Math.random()*m);
+		}
 	</script>
 </body>
 </html>
