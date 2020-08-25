795818ec9ff3f8b5c1fe03c2be0c50d46b85b78e
Jonathan Soto <jonathansoto@iMac-de-Jonathan.local>
2020-08-25 12:13:57 -0600

    implementando walls

--- index.php
+++ index.php
@@ -25,7 +25,8 @@
 	</div>
 	<script type="text/javascript">
 		var canvas = null, ctx = null, x = 0, y = 0;
-		var lastPress = 65, speed = 5, player = null, food =null;
+		var lastPress = 65, speed = 2, player = null, food =null;
+		var score = 0, pause = false, walls = new Array();
 
 		window.requestAnimationFrame = (function(){
 			return window.requestAnimationFrame || 
@@ -47,44 +48,66 @@
 			ctx.fillStyle = "red";
 			food.paint(ctx);
 
-			// ctx.fillStyle = "#0f0";
-			// ctx.fillRect(x,y,10,10);
+			ctx.fillStyle = "white";
+			ctx.textAlign = "left";
+			ctx.fillText("SCORE: "+score+" SPEED: "+speed,10,10);
+
+			ctx.fillStyle = "#9E9A99";
+			for (var i = walls.length - 1; i >= 0; i--) {
+				walls[i].paint(ctx);
+			}
+
+			if(pause){
+				ctx.fillStyle = "white";
+				ctx.textAlign = "center";
+				ctx.fillText(" P A U S E ",240,120);
+			}
 		}
 
-		function act(){
- 
+		function act(){ 
 
-			if(lastPress==65 || lastPress == 37){ //izquierda
-				player.x -= speed;
-				if (player.x < 0 ) {
-					player.x = canvas.width;
+			if(!pause){ 
+
+				if(lastPress==65 || lastPress == 37){ //izquierda
+					player.x -= speed;
+					if (player.x < 0 ) {
+						player.x = canvas.width;
+					}
 				}
-			}
 
-			if(lastPress==68 || lastPress == 39){ //derecha
-				player.x += speed;
-				if(player.x > canvas.width){
-					player.x = -10;
+				if(lastPress==68 || lastPress == 39){ //derecha
+					player.x += speed;
+					if(player.x > canvas.width){
+						player.x = -10;
+					}
 				}
-			}
 
-			if(lastPress==87 || lastPress == 38){ //arriba
-				player.y -= speed;
-				if (player.y < 0) {
-					player.y = canvas.height;
+				if(lastPress==87 || lastPress == 38){ //arriba
+					player.y -= speed;
+					if (player.y < 0) {
+						player.y = canvas.height;
+					}
 				}
-			}
 
-			if(lastPress==83 || lastPress == 40){ //abajo
-				player.y += speed;
-				if (player.y > canvas.height) {
-					player.y = -10;
+				if(lastPress==83 || lastPress == 40){ //abajo
+					player.y += speed;
+					if (player.y > canvas.height) {
+						player.y = -10;
+					}
+				} 
+
+				if(player.intersects(food)){
+					food.x = random(canvas.width);
+					food.y = random(canvas.height);
+					score += 10;
+					speed += 1;
 				}
-			} 
 
-			if(player.intersects(food)){
-				food.x = random(canvas.width);
-				food.y = random(canvas.height);
+				for (var i = walls.length - 1; i >= 0; i--) {
+					if(walls[i].intersects(player)){
+						//validar para hacerlo perder
+					}
+				}
 			}
 		}
 
@@ -101,6 +124,12 @@
 			player = new Rectangle(0,0,10,10);
 			food = new Rectangle(70,50,10,10);
 
+			walls.push(new Rectangle(100,50,10,10));
+			walls.push(new Rectangle(100,200,10,10));
+
+			walls.push(new Rectangle(400,50,10,10));
+			walls.push(new Rectangle(400,200,10,10));
+
 			run();
 		} 
 		
@@ -109,7 +138,15 @@
 		document.addEventListener('keydown',function(e){ 
 			if(e.keyCode==65 || e.keyCode ==  37 || e.keyCode ==  68 || e.keyCode ==  39 || e.keyCode ==  87 || e.keyCode ==  38 || e.keyCode ==  83 || e.keyCode ==  40 )
 				lastPress = e.keyCode; 
+			else if(e.keyCode==32){
+
+				pause = (pause)?false:true; 
+			}
 		})
+
+		function test(){
+			console.log("Test")
+		}
 
 		function Rectangle(x,y,w,h){
 			this.x = x;
