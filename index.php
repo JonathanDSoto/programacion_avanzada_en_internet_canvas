920b0ad998c309827f49a651c1f87d4931c0ee00
Jonathan Soto <>
2020-09-24 09:10:30 -0600

    bootstrap implementatio

--- index.php
+++ index.php
@@ -1,203 +1,385 @@
-<?php 
-	$info = "";
- 	#$info = file_get_contents('https://jsonplaceholder.typicode.com/todos/');
-	
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>
-		Canvas
+		Bootstrap
 	</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> 
 	<style type="text/css">
-		.container{
-			width: 80%;
-			background-color: gray;
-			margin: auto;
-			min-height: 200px;
-		} 
-		canvas{
-			background-color: #F7D358;
-		} 
-		.table{
-			width: 30%;
-			margin: auto;
-			background-color: #ff000094;
-			border-radius: 5px;
-			display: inline-block;
+		/* Footer */
+		@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		section {
+		    padding: 60px 0;
 		}
-		p{
-			text-align: center;
+
+		section .section-title {
+		    text-align: center;
+		    color: #007b5e;
+		    margin-bottom: 50px;
+		    text-transform: uppercase;
 		}
-		.amarilloso{
-			color:yellow !important;
+		#footer {
+		    background: #007b5e !important;
 		}
+		#footer h5{
+			padding-left: 10px;
+		    border-left: 3px solid #eeeeee;
+		    padding-bottom: 6px;
+		    margin-bottom: 20px;
+		    color:#ffffff;
+		}
+		#footer a {
+		    color: #ffffff;
+		    text-decoration: none !important;
+		    background-color: transparent;
+		    -webkit-text-decoration-skip: objects;
+		}
+		#footer ul.social li{
+			padding: 3px 0;
+		}
+		#footer ul.social li a i {
+		    margin-right: 5px;
+			font-size:25px;
+			-webkit-transition: .5s all ease;
+			-moz-transition: .5s all ease;
+			transition: .5s all ease;
+		}
+		#footer ul.social li:hover a i {
+			font-size:30px;
+			margin-top:-10px;
+		}
+		#footer ul.social li a,
+		#footer ul.quick-links li a{
+			color:#ffffff;
+		}
+		#footer ul.social li a:hover{
+			color:#eeeeee;
+		}
+		#footer ul.quick-links li{
+			padding: 3px 0;
+			-webkit-transition: .5s all ease;
+			-moz-transition: .5s all ease;
+			transition: .5s all ease;
+		}
+		#footer ul.quick-links li:hover{
+			padding: 3px 0;
+			margin-left:5px;
+			font-weight:700;
+		}
+		#footer ul.quick-links li a i{
+			margin-right: 5px;
+		}
+		#footer ul.quick-links li:hover a i {
+		    font-weight: 700;
+		}
+
+		@media (max-width:767px){
+			#footer h5 {
+		    padding-left: 0;
+		    border-left: transparent;
+		    padding-bottom: 0px;
+		    margin-bottom: 10px;
+		}
+		}
+
 	</style>
 </head>
 <body>
-	<div class="container">  
 
-		<div class="table">
-			<h1>
-				Encabezado
-			</h1>
+	<div class="container">
 
-			<p class="parrafo_1">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br> 
-			</p>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <a class="navbar-brand" href="#">
+		  	Guacamole
+		  </a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
 
-			<button class="uno">
-				Presioname
-			</button>
-		</div> 
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">
+		        	Dashboard 
+		        	<span class="sm-only">(current)</span>
+		        </a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">
+		        	Usuarios
+		        </a>
+		      </li> 
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Escribe aquí" aria-label="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+		      	Buscar
+		      </button>
+		    </form>
+		  </div>
+		</nav>
 
-		<div class="table">
-			<h1 id="title_2" style="">
-				Encabezado
-			</h1>
+		<nav aria-label="breadcrumb ">
+		  <ol class="breadcrumb">
+		    <li class="breadcrumb-item active" aria-current="page">Home</li>
+		  </ol>
+		</nav>
 
-			<p class="parrafo_2" style="display: none;">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>  
-			</p>
+		<div class="jumbotron">
 
-			<button class="dos">
-				Presioname
-			</button>
-		</div> 
+		  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img src="assets/images/aguacate.png" class="d-block w-100 h-20" alt="...">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="assets/images/receta-de-guacamole-sin-tomate.jpg" class="d-block w-100 h-20" alt="...">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="assets/images/shutterstock-768061894.jpg" class="d-block w-100 h-20" alt="...">
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
 
-		<div class="table">
-			<h1>
-				Encabezado
-			</h1>
+		</div>
 
-			<p id="parrafo">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>
-				
-			</p>
+		<div class="row mt-5">
+			<div class="card-deck">
+			  <div class="card">
+			    <img src="assets/images/aguacate.png" class="card-img-top" alt="...">
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			    </div>
+			    <div class="card-footer">
+			      <small class="text-muted">Last updated 3 mins ago</small>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img src="assets/images/aguacate.png" class="card-img-top" alt="...">
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+			    </div>
+			    <div class="card-footer">
+			      <small class="text-muted">Last updated 3 mins ago</small>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <img src="assets/images/aguacate.png" class="card-img-top" alt="...">
+			    <div class="card-body">
+			      <h5 class="card-title">Card title</h5>
+			      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+			    </div>
+			    <div class="card-footer">
+			      <small class="text-muted">Last updated 3 mins ago</small>
+			    </div>
+			  </div>
+			</div>
+		</div>
 
-			<button class="tres">
-				Presioname
-			</button>
-		</div> 
-
-		<hr>
-
-		<button id="get">
-			Get
-		</button>
-
-		<button id="ajax">
-			Ajax
-		</button>
-
-		<button id="script">
-			Script
-		</button>
-
-		<button id="json">
-			Json
-		</button>
-
-	</div> 
-
-
-	<script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
-	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function(){ 
-
-			$("#get").click(function(){
-				// console
-				$.get('https://jsonplaceholder.typicode.com/todos/',
-					function(r){
-						console.log(r)
-						//$(".container").append(r)
-					})
-			})
-
-			$("#ajax").click(function(){
-				$.ajax({ 
-    				url : './post.php', 
-    				data:{ token:'asd1902jads',otro:1 },
-    				type : 'POST', 
-    				dataType : 'json',
-    				success:function(r){
-    					console.log(r)
-    				},
-    				error:function(xhr,status)
-    				{
-    					console.log(xhr)
-    					console.log(status)
-    				},
-    				complete:function(xhr,status)
-    				{
-    					
-    				}
-				})
-				// axios.post('post.php', {
-				//     token: 'asd1902jads'
-				//   })
-				//   .then(function (response) {
-				//     console.log(response);
-				//   })
-				//   .catch(function (error) {
-				//     console.log(error);
-				//   });
-			})
-
-			$("#script").click(function(){
-				$.getScript('assets/js/jquery-3.5.1.min.js', function() {
-				    console.log("hola")
-				});
-			})
-
-			$("#json").click(function(){
-				$.getJSON('https://jsonplaceholder.typicode.com/todos/', function(resp) {
-				    $.each(resp, function(k, v) {
-				        console.log(k + ' : ' + v);
-					}); 
-				});
-			})
-
-			$('button.uno').click(function(){
-
-				$('.parrafo_1').animate(
-				    { 
-				        opacity : '-=0.25',
-				        fontSize: '+=5px'
-				    },
-				    900, // duration
-				 
-				    // función de devolución de llamada
-				    function() { console.log('realizado');
-				});
-
-			}) 
-
-			$(".dos").click(function(){
-				$(".parrafo_2").slideDown()
-			})
+		<div class="row mt-5 ">
 			
-			$(".tres").click(function(){
-				$("#parrafo").slideUp(4000)
-			})
+			<div class="col"> 
+			
+				<div class="card">
+				  <div class="card-header">
+				    
+				    Tabla de usuarios registrados
 
-		}) 
-	</script> 
+				    <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+				    	Añadir usuario
+				    </button>
+
+				  </div>
+				  <div class="card-body"> 
+				  	<table class="table table-bordered table-striped">
+					  <thead class="thead-dark">
+					    <tr>
+					      <th scope="col">#</th>
+					      <th scope="col">First</th>
+					      <th scope="col">Last</th>
+					      <th scope="col">Handle</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					    <tr>
+					      <th scope="row">1</th>
+					      <td>Mark</td>
+					      <td>Otto</td>
+					      <td>@mdo</td>
+					    </tr>
+					    <tr>
+					      <th scope="row">2</th>
+					      <td>Jacob</td>
+					      <td>Thornton</td>
+					      <td>@fat</td>
+					    </tr>
+					    <tr>
+					      <th scope="row">3</th>
+					      <td>Larry</td>
+					      <td>the Bird</td>
+					      <td>@twitter</td>
+					    </tr>
+					  </tbody>
+					</table> 
+				  </div>
+				</div>
+
+			</div>
+
+		</div>
+		
+		<div class="row mt-5">
+			<!--  -->
+			<div class="col-sm-4">
+				<h1>
+					Título del post
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="">
+					Leer más
+				</a>
+			</div> 
+
+			<div class="col-sm-4">
+				<h1>
+					Título del post
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="">
+					Leer más
+				</a>
+			</div> 
+
+			<div class="col-sm-4">
+				<h1>
+					Título del post
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="">
+					Leer más
+				</a>
+			</div> 
+			
+		</div>
+
+		<section id="footer">
+			<div class="container">
+				<div class="row text-center text-xs-center text-sm-left text-md-left">
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+							<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
+							<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+						<ul class="list-unstyled list-inline social text-center">
+							<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+							<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+						</ul>
+					</div>
+					<hr>
+				</div>	
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+						<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
+						<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+					</div>
+					<hr>
+				</div>	
+			</div>
+		</section>
+		<!-- ./Footer -->
+
+
+	</div>
+
+	<div class="container">
+		
+	</div>
+
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        ...
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 </body>
 </html>
--- post.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	
-	if (isset($_POST['year']) && $_POST['year']!="") {
-		$edad = 2020 - $_POST['year'];
-		echo $edad;
-	}
-	
-?>
